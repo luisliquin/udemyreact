0de104997663f0530794015342a6a4aa2e4abb7d
Luis Liquin <luisliquin@outlook.com.ar>
2023-05-30 23:50:19 -0300

    fin etapa 4

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,13 +1,28 @@
+import { useState } from 'react'
 import PropTypes from 'prop-types'
 
 export const CounterApp = ({value}) => {
+    const [counter, setCounter] = useState(value);
+    
+    const handleAdd = () =>{
+        setCounter(counter + 1);
+    }
+
+    const handleSubstrack = () =>{
+        setCounter(counter - 1);
+    }
+    
+    const handleReset = () =>{
+        setCounter(value);
+    }
+
     return (
     <>
         <h1>CounterApp</h1>
-        <h2>{value}</h2>
-        <button onClick={function() {console.log('+1')}}>
-            +1
-        </button>
+        <h2>{counter}</h2>
+        <button onClick={handleAdd}>+1</button>
+        <button onClick={handleSubstrack}>-1</button>
+        <button onClick={handleReset}>Reset</button>
     </>
 )}
 
